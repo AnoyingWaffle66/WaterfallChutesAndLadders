376aa7d10182784f462141ce45cfeadec93e7dd4
Josh Arbon <jarbon@student.neumont.edu>
2025-01-19 17:23:27 -0700

    dice structure

--- waterfallChutesAndLadders/Dice.cs
+++ waterfallChutesAndLadders/Dice.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace waterfallChutesAndLadders
+﻿namespace waterfallChutesAndLadders
 {
     internal class Dice
     {
+        public static int roll(int low, int high)
+        {
+            return 0;
+        }
     }
 }
