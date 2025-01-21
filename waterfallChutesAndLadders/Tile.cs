1517a68bbd5f6eb00e512523f8c89c9244e32592
Josh Arbon <jarbon@student.neumont.edu>
2025-01-21 16:11:12 -0700

    test2

--- waterfallChutesAndLadders/Tile.cs
+++ waterfallChutesAndLadders/Tile.cs
@@ -23,6 +23,7 @@
         public int Go_to_position
         {
             get { return go_to_position; }
+            set { this.go_to_position = value; }
         }
     }
     public enum TileType
