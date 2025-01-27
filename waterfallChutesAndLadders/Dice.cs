d05f9048a23fac8be0a1b472bed9c577be489b7f
Josh Arbon <jarbon@student.neumont.edu>
2025-01-27 13:06:37 -0700

    dryest dice

--- waterfallChutesAndLadders/Dice.cs
+++ waterfallChutesAndLadders/Dice.cs
@@ -4,26 +4,14 @@
     {
         private static Random rand = new Random();
 
-        public static int roll()
-        {
-            //Assumes rolling one six sided die.
-            return rand.Next(1, 7);
-        }
-
-        public static int roll(int low, int high)
-        {
-            //Inputting roll(1, 6) will act as a six sided die.
-            return rand.Next(low, high + 1);
-        }
-
-        public static int roll(int low, int high, int amount)
+        public static int roll(int low = 1, int high = 6, int amount = 1)
         {
             //Inputting roll(1, 6, 2) will act as a six sided die 2 times and return the total.
             int total = 0;
 
             for (int i = 0; i < amount; i++)
             {
-                total += roll(low, high);
+                total += rand.Next(low, high + 1);
             }
 
             return total;
