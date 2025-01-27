ff94f23b657dc6b22b31c2a5ba1fab6678a2a81f
JBrincefield <jbrincefield@student.neumont.edu>
2025-01-27 15:45:09 -0700

    looks better
    title

--- waterfallChutesAndLadders/Game.cs
+++ waterfallChutesAndLadders/Game.cs
@@ -30,17 +30,31 @@
             // Update the current player's position via a roll of a six sided die.
             player.Position += player_roll;
             int p_position = player.Position;
-            handle_tile(player, board[p_position / board_width, p_position % board_width]);
+            handle_tile(player, board[p_position / board_width, p_position % board_width], player_roll);
 
         }
 
-        public void handle_tile(Player player, Tile tile)
+        public void handle_tile(Player player, Tile tile, int player_roll)
         {
             switch (tile.Tile_type)
             {
                 case TileType.LADDER:
+                    if (player.Position == tile.Activate_position)
+                    {
+                        Console.WriteLine($"You've landed on a ladder by rolling a {player_roll}! You're moving up to position {tile.Go_to_position + 1}");
+                        Console.WriteLine("Press enter to continue!");
+                        Console.ReadLine();
+                        player.Position = tile.Go_to_position;
+                    }
+                    break;
                 case TileType.CHUTE:
-                    player.Position = tile.Go_to_position;
+                    if (player.Position == tile.Activate_position)
+                    {
+                        Console.WriteLine($"You've landed on a chute by rolling a {player_roll}! You're moving down to position {tile.Go_to_position + 1}");
+                        Console.WriteLine("Press enter to continue!");
+                        Console.ReadLine();
+                        player.Position = tile.Go_to_position;
+                    }
                     break;
                 case TileType.BLANK:
                     break;
